17981d78937efc115754fb6aca92873ee20a215d
J2d6 <j2d6.pro@gmail.com>
2024-04-02 22:52:01 +0300

    Add main() on Femme

--- TP_GLOG_M1/src/iut/bad/Femme.java
+++ TP_GLOG_M1/src/iut/bad/Femme.java
@@ -4,4 +4,10 @@ public class Femme extends Humain {
 	public Femme( String nom, String prenom, int age ) {
 		super( nom, prenom, age );
 	}
+	
+	public static void main (String[] args) {
+		Homme lehilahy  = new Homme("RAZ", "Dimbiniaina Jordany", 23);
+		Femme vehivavy = new Femme("ZAFY", "Marie ", 24);
+		vehivavy.ami(lehilahy);
+	}
 }
