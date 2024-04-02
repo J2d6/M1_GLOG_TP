ebd9197f1bff43ae22300fb429c2f887887b0922
J2d6 <j2d6.pro@gmail.com>
2024-04-02 22:15:29 +0300

    Add toString() method to Humain class

--- TP_GLOG_M1/src/iut/bad/Humain.java
+++ TP_GLOG_M1/src/iut/bad/Humain.java
@@ -12,6 +12,10 @@ public class Humain {
 	}
 	
 	public void details () {
-		System.out.println("NOM : " + this.nom + "\n" +"Prenom : " + this.prenom + "\n" + "Age : " + this.age);
+		System.out.println(toString());
+	}
+	
+	public String toStirng () {
+		return ( "NOM : " + this.nom + "\n" +"Prenom : " + this.prenom + "\n" + "Age : " + this.age );
 	}
 }
