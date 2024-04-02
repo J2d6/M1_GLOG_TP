9ab8c0fdf56dc82ba733bb9eb8b87a3356a189d1
J2d6 <j2d6.pro@gmail.com>
2024-04-02 22:00:30 +0300

    Add Homme class

---
+++ TP_GLOG_M1/src/iut/bad/Homme.java
@@ -0,0 +1,15 @@
+package iut.bad;
+
+public class Homme {
+	String nom;
+	String prenom;
+	int age;
+	
+	public Homme( String nom, String prenom, int age ) {
+		this.nom = nom;
+		this.prenom = prenom;
+		this.age = age;
+	}
+	
+	
+}
